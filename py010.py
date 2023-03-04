45b16abb4818e2573eec70c11885ad0a734b3282
cazuike <chidi15azuike@gmail.com>
2023-03-04 09:10:15 -0500

    early mo

--- py010.py
+++ py010.py
@@ -1,5 +1,15 @@
 
 def main():
+    g,a,b = map(int, input().split(" "))
+    l = 0
+    r = 10**18
+    while r > l:
+        mid = (l+r)//2
+        if mid//a + mid//b >= (g+1):
+            r = mid
+        else:
+            l = mid+1
+    print(l)
             
  
 if __name__ == "__main__":
