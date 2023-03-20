7dba9c0a91ab994a5a8b3613283fdc20b8ff9ade
cazuike <chidi15azuike@gmail.com>
2023-03-19 21:46:58 -0400

    revised

--- py011.py
+++ py011.py
@@ -3,14 +3,24 @@ def main():
     for _ in range(args):
         L = int(input())
         arr = list(map(int, input().split(" ")))
-        mval = list(map(abs,arr))
-        
-        if L == 2:
-            print(max(sum(arr),-1*sum(arr)))
-            continue
-        res = sum(arr)
+        abs_sum = 0
+        rsum = 0
+        ngc = 0 
         for i in range(L):
-            res = max(res,sum(mval)-2*abs(arr[i]))
+            if arr[i] < 0:
+                ngc += 1
+            abs_sum += abs(arr[i])
+            rsum += arr[i]
+
+        if L == 2:
+            print(max(abs_sum,rsum))
+            continue
+        if ngc % 2 == 0:
+            print(abs_sum)
+            continue
+        res = rsum
+        for i in range(L):
+            res = max(res,abs_sum-2*abs(arr[i]))
         print(res)
     return True
 	
